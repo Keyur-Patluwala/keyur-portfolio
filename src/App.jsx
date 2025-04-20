ab6abebc46f16c85a3407cb191d55afa177d40c6
Keyur Patluwala <keyurpatluwala@gmail.com>
2025-04-20 13:31:44 +0530

    added project section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
+// import Experience from "./sections/Experience.jsx";
+import WorkExperience from "./sections/Experience.jsx";
 
 const App = () => {
   return (
@@ -15,7 +17,7 @@ const App = () => {
       <About />
       <Projects />
       <Clients />
-
+      <WorkExperience />
       <Contact />
       <Footer />
     </main>
